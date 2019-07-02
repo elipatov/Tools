1bd676893830dc476b4e50c59de955b65a33f32c
elipatov <evgeni.lipatov@gmail.com>
2019-07-02 19:01:24 +0300

    Add ProducerConsumerStream (#3)
    * Add ProducerConsumerStream
    
    * Get rid of legacy

---
+++ src/Libs/Async/AsyncConditionVariable.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Libs.Async
+{
+    public class AsyncConditionVariable
+    {
+        private const int TRUE = 1;
+        private const int FALSE = 0;
+        private TaskCompletionSource<bool> _completion = new TaskCompletionSource<bool>();
+        private int _hasAwaiters;
+        private bool _isClosed;
+
+        public void NotifyAll()
+        {
+            bool hasAwaiters = Interlocked.CompareExchange(ref _hasAwaiters, FALSE, TRUE) != FALSE;
+            if (hasAwaiters)
+            {
+                var completion = Volatile.Read(ref _isClosed) 
+                    ? Volatile.Read(ref _completion) 
+                    : Interlocked.Exchange(ref _completion, new TaskCompletionSource<bool>());
+
+                if (!completion.Task.IsCompleted)
+                {
+                    Task.Factory.StartNew(s => ((TaskCompletionSource<bool>) s).TrySetResult(true),
+                        completion, CancellationToken.None, TaskCreationOptions.PreferFairness, TaskScheduler.Default);
+                    completion.Task.Wait();
+                }
+            }
+        }
+
+        public async Task WaitAsync(CancellationToken token)
+        {
+            Volatile.Write(ref _hasAwaiters, TRUE);
+            TaskCompletionSource<bool> completion = Volatile.Read(ref _completion);
+            using (token.Register(() => { completion.TrySetCanceled(token); }, false))
+            {
+                await completion.Task.ConfigureAwait(false);
+            }
+        }
+
+        public void Close()
+        {
+            Volatile.Write(ref _isClosed, true);
+            NotifyAll();
+        }
+    }
+}
---
+++ src/Tests/ProducerConsumerStreamTest.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Libs.ProducerConsumerStream;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace Tests
+{
+    [TestFixture]
+    public class ProducerConsumerStreamTest
+    {
+        private Random _rnd;
+
+        [SetUp]
+        public void Setup()
+        {
+            _rnd = new Random();
+        }
+
+        [TestCase(40)]
+        [TestCase(255)]
+        [TestCase(257)]
+        [TestCase(4096)]
+        [TestCase(16 * 1024)]
+        public async Task Should_Copy_Sync(int size)
+        {
+            for (int i = 0; i < 10; ++i)
+            {
+                ProducerConsumerStream stream = new ProducerConsumerStream(64 * 1024);
+                byte[] original = GenerateTestData(size);
+                await StartProducer(size, original, stream.Writer);
+
+                byte[] actual = ToArray(stream.Reader);
+                actual.Should().BeEquivalentTo(original);
+            }
+
+        }
+
+        [TestCase(40)]
+        [TestCase(255)]
+        [TestCase(257)]
+        [TestCase(16 * 1024)]
+        public void Should_Copy_Async(int size)
+        {
+            for (int i = 0; i < 10; ++i)
+            {
+                ProducerConsumerStream stream = new ProducerConsumerStream(1024);
+                byte[] original = GenerateTestData(size);
+                Task producerTask = StartProducer(size, original, stream.Writer);
+
+                byte[] actual = ToArray(stream.Reader);
+                actual.Should().BeEquivalentTo(original);
+            }
+        }
+
+        [Test]
+        [Ignore("Should_Not_Deadlock")]
+        public async Task Should_Not_Deadlock_On_Contuniation()
+        {
+            int size = 16 * 1024;
+            ProducerConsumerStream stream = new ProducerConsumerStream(1024);
+            byte[] original = GenerateTestData(8);
+            Task producerTask = StartCopingProducer(size, original, stream.Writer, true);
+            Task consumerTask = StartConsumerAsync(stream, 512, true);
+            await Task.WhenAll(producerTask, consumerTask);
+        }
+
+        [Test]
+        [Ignore("Should_Not_Deadlock")]
+        public async Task Should_Not_Deadlock_On_Ping_Pong()
+        {
+            int size = 1024 * 1024 * 1024;
+            ProducerConsumerStream stream = new ProducerConsumerStream(512);
+            byte[] original = GenerateTestData(300);
+            Task producerTask = StartCopingProducer(size, original, stream.Writer, false);
+            Task consumerTask = StartConsumerAsync(stream, 4096, false);
+            await Task.WhenAll(producerTask, consumerTask);
+        }
+
+        private async Task StartProducer(int size, byte[] original, Stream writeStream)
+        {
+            int position = 0;
+
+            do
+            {
+                int chunkSize = _rnd.Next(1, Math.Abs(size - position) / 2 + 1) + 4;
+                await writeStream.WriteAsync(original, position, chunkSize);
+                position += chunkSize;
+            } while (position < size);
+
+            writeStream.Dispose();
+        }
+
+        private async Task StartCopingProducer(int size, byte[] original, Stream writeStream, bool delay)
+        {
+            await Task.Yield();
+            int position = 0;
+
+            do
+            {
+                if(delay) await Task.Delay(5);
+                await writeStream.WriteAsync(original, 0, original.Length);
+                position += original.Length;
+            } while (position < size);
+
+            writeStream.Dispose();
+        }
+
+        private async Task StartConsumerAsync(ProducerConsumerStream stream, int bufferSize, bool wait)
+        {
+            await Task.Yield();
+            byte[] buffer = new byte[bufferSize];
+            int count;
+
+            do
+            {
+                if(wait) await stream.WaitAvailableBytesAsync(bufferSize, CancellationToken.None);
+                count = await stream.Reader.ReadAsync(buffer, 0, bufferSize);
+            } while (count == bufferSize);
+        }
+
+        private byte[] GenerateTestData(int size)
+        {
+            byte[] original = new byte[size];
+            _rnd.NextBytes(original);
+            original[size - 1] = (byte)_rnd.Next(1, 255);
+            original[0] = (byte)_rnd.Next(1, 255);
+            return original;
+        }
+
+        private static byte[] ToArray(Stream stream)
+        {
+            using (var readStream = new MemoryStream())
+            {
+                stream.CopyTo(readStream);
+                byte[] plain = readStream.ToArray();
+                return plain;
+            }
+        }
+    }
+}
